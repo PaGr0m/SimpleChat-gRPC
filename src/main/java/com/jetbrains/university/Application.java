b08d02f77912706a928e5b5eb22306506bdb17f3
Pavel Gromov <pagrom27@gmail.com>
2020-05-13 14:37:55 +0300

    Create proto

--- src/main/java/com/jetbrains/university/Application.java
+++ src/main/java/com/jetbrains/university/Application.java
@@ -1,7 +1,15 @@
 package com.jetbrains.university;
 
+
+import com.jetbrains.university.MessageResponseOuterClass.MessageResponse;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Start");
+        MessageResponse messageResponse = MessageResponse.newBuilder()
+                                                         .setDate("asd")
+                                                         .setMessage("qwe")
+                                                         .setPersonName("ZhekehZ")
+                                                         .build();
     }
 }
